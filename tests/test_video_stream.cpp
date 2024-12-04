dfef373bca13ba653795427ff703dd677e2e201f
awkrail <taichitary@gmail.com>
2024-12-04 16:25:36 +0900

    fix path

--- tests/test_video_stream.cpp
+++ tests/test_video_stream.cpp
@@ -6,7 +6,7 @@
 using namespace video_stream;
 
 TEST_CASE("VideoStream initialization", "[VideoStream init]") {
-    std::string input_path = "../video/input.mp4";
+    std::string input_path = "../../video/input.mp4";
     VideoStream video_stream = VideoStream(input_path);
     REQUIRE(std::abs(video_stream.get_framerate() - 29.97f) < 0.001);
 }
